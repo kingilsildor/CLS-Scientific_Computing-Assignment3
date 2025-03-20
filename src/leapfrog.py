a7c52f96c8a720a297fe35c6bbe605af818954aa
Anezka <anezkapotesilova@gmail.com>
2025-03-20 10:47:01 +0100

    Plotting multiple k

--- src/leapfrog.py
+++ src/leapfrog.py
@@ -1,12 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# TODO: delete after we start using np and plt in the code
-blah = np.sum([1, 2, 3])
-fig = plt.plot()
 
-
-def leapfrog(T, F_x, delta_t=0.01, F_t=None, m=1):
+def leapfrog(T, F_x, k, delta_t=0.01, F_t=None, m=1):
     num_time_steps = int(T // delta_t)
 
     x_list = np.zeros(num_time_steps + 1)
@@ -14,18 +10,26 @@ def leapfrog(T, F_x, delta_t=0.01, F_t=None, m=1):
 
     x_list[0] = 1  # Initial position of the mass at t = 0
     v_list[0] = (
-        delta_t * F_x(x_list[0]) / m
+        delta_t * F_x(x_list[0], k) / m
     )  # Initial velocity of the mass at t = 1/2 * delta_t
     # v_list[0] = 0
 
+    if F_t is None:
+
+        def F_t(t, A, omega):
+            return 0
+
     for i in range(num_time_steps):
         x_list[i + 1] = x_list[i] + delta_t * v_list[i]
-        v_list[i + 1] = v_list[i] + delta_t * F_x(x_list[i + 1]) / m
+        v_list[i + 1] = (
+            v_list[i]
+            + delta_t * (F_x(x_list[i + 1], k) + F_t((i + 1) * delta_t, 0.1, 0.2)) / m
+        )
 
     return x_list, v_list
 
 
-def plot_leapfrog(x_list, v_list, T, k, m=1, detail="Position"):
+def plot_leapfrog(x_list, v_list, T, k, m=1):
     # Get exact anlytical solution
     t = np.linspace(0, T, len(x_list))
     A = 1
@@ -43,10 +47,42 @@ def plot_leapfrog(x_list, v_list, T, k, m=1, detail="Position"):
 
     # plt.plot(np.abs(exact_sol - x_list))
     plt.plot(exact_sol - x_list)
+    plt.title("Error in Position vs Time")
+    plt.xlabel("Time")
+    plt.ylabel("Error in Position")
     plt.show()
 
     plt.plot(t, v_list)
     plt.title("Velocity vs Time")
     plt.xlabel("Time")
     plt.ylabel("Velocity")
+    plt.show()
+
+
+def plot_leapfrog_various_k(positions, velocities, k_values, T, save=False):
+    t = np.linspace(0, T, len(positions[0]))
+    colors = ["r", "g", "y"]
+
+    # Positions
+    for i, k in enumerate(k_values):
+        plt.plot(t, positions[i], label=f"k = {k}", color=colors[i])
+
+    plt.title("Position vs Time")
+    plt.xlabel("Time")
+    plt.ylabel("Position")
+    plt.legend()
+    if save:
+        plt.savefig("results/leapfrog_position.png")
+    plt.show()
+
+    # Velocities
+    for i, k in enumerate(k_values):
+        plt.plot(t, velocities[i], label=f"k = {k}", color=colors[i])
+
+    plt.title("Velocity vs Time")
+    plt.xlabel("Time")
+    plt.ylabel("Velocity")
+    plt.legend()
+    if save:
+        plt.savefig("results/leapfrog_velocity.png")
     plt.show()
